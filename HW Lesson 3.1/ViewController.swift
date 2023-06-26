9974ae541c58e06771144704aa8704f1652e91d6
ChauveSouris <desseobscurite@gmail.com>
2023-06-26 23:42:30 +0300

    Created Storiboard and VC

--- HW Lesson 3.1/ViewController.swift
+++ HW Lesson 3.1/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var animationView: UIView!
+    
+    @IBOutlet var titleAnimationLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func animationButton(_ sender: UIButton) {
+    }
 }
 
