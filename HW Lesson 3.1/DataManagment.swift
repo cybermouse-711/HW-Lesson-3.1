2fe41f8d4f25bfb02aac91009461c3c9325023fd
ChauveSouris <desseobscurite@gmail.com>
2023-06-26 23:48:45 +0300

    Created DataManagment

---
+++ HW Lesson 3.1/DataManagment.swift
@@ -0,0 +1,16 @@
+//
+//  DataManagment.swift
+//  HW Lesson 3.1
+//
+//  Created by Елизавета Медведева on 26.06.2023.
+//
+
+class Animations {
+    
+    static let shared = Animations()
+    
+    let preset = ["preset"]
+    let curve = ["curve"]
+    
+    private init() {}
+}
